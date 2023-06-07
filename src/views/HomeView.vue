fdc8b1073befd680ec938ac1f8090c7e0ad24e2b
marc <marcgc21@gmail.com>
2023-06-07 16:10:24 -0300

    dummy axios get request to /api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <p>MY PATIENTS</p>
+    <p>{{dicoms}}</p>
+    <button @click="callEndpoint()">GET DICOMS</button>
   </main>
 </template>
+
+<script >
+import axios from 'axios'
+
+export default{
+  data(){
+    return {
+      dicoms:'HELLO NATA',
+    }
+  },
+  
+  methods:{
+    async callEndpoint(){
+      this.dicoms="hehehe"
+      await axios.get('http://localhost:7171/api/lg')
+      .then(response => {
+        // Handle the response data
+        console.log(response.data);
+        this.dicoms=response.data
+      })
+      .catch(error => {
+        // Handle the error
+        console.error(error);
+      });
+  
+    }
+  }
+}
+
+</script>
