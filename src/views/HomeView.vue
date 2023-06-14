3c219db257e3341e1b22afad0e61c1e2de10ee30
Natanco <nata.oliveira2004@gmail.com>
2023-06-14 12:03:15 -0300

    feat: socket conection

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,42 @@
 <template>
   <main>
     <p>MY PATIENTS</p>
-    <p>{{dicoms}}</p>
+    <p>{{ dicoms }}</p>
     <button @click="callEndpoint()">GET DICOMS</button>
   </main>
 </template>
 
 <script >
 import axios from 'axios'
+import io from 'socket.io-client'
+const socket = io.connect('http://localhost:7171/')
 
-export default{
-  data(){
+export default {
+  data() {
     return {
-      dicoms:'HELLO NATA',
+      dicoms: 'HELLO NATA',
     }
   },
-  
-  methods:{
-    async callEndpoint(){
-      this.dicoms="hehehe"
-      await axios.get('http://localhost:7171/api/lg')
-      .then(response => {
-        // Handle the response data
-        console.log(response.data);
-        this.dicoms=response.data
-      })
-      .catch(error => {
-        // Handle the error
-        console.error(error);
-      });
-  
+  created() {
+    socket.on('change', (change) => {
+      console.log(`something changed: ${change}`)
+    })
+  },
+
+  methods: {
+    async callEndpoint() {
+      this.dicoms = "hehehe"
+      await axios.get('http://localhost:7171/api/patient')
+        .then(response => {
+          // Handle the response data
+          console.log(response.data);
+          this.dicoms = response.data
+        })
+        .catch(error => {
+          // Handle the error
+          console.error(error);
+        });
+
     }
   }
 }
