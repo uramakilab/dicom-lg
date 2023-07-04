203ea0af2bbae7d6f745b103037faca1d203252f
Natanco <nata.oliveira2004@gmail.com>
2023-07-04 11:58:54 -0300

    feat: getting images from bakc end

--- dicom-lg-vue/src/components/image/ImageViewComponent.vue
+++ dicom-lg-vue/src/components/image/ImageViewComponent.vue
@@ -1,6 +1,7 @@
 <template>
-    <v-row v-for="n in 3" :key="n" class="d-flex child-flex" justify="center" align="center" cols="3" style="height: 33vh;">
-        <v-img class="px-16 py-5" :src="getPath(n)" style="height: 100%;"></v-img>
+    <v-row v-for="n in this.images" :key="n" class="d-flex child-flex" justify="center" align="center" cols="3"
+        style="height: 33vh;">
+        <v-img class="px-16 py-5" :src="getImageSrc(n)" style="height: 100%;"></v-img>
     </v-row>
 </template>
   
@@ -28,13 +29,23 @@ export default {
             console.log(change);
             await lgPosition.getPositions();
             console.log(JSON.parse(JSON.stringify(this.positioning)));
+            location.reload()
+        },
+        getImageSrc(n) {
+            const imageBuffer = JSON.parse(JSON.stringify(n));
+            // console.log('Image Buffer:', imageBuffer);
+
+            const base64Data = btoa(String.fromCharCode.apply(null, imageBuffer.data));
+            const dataUrl = `data:image/png;base64,${base64Data}`;
+            // console.log('Data URL:', dataUrl);
+
+            return dataUrl;
         }
     },
     async created() {
         socket.on('change', this.handlePositionChange);
         await lgPosition.getImages('6495cc254e05748bf50aa15b');
-        console.log(JSON.parse(JSON.stringify(this.images)));
-
+        // console.log(JSON.parse(JSON.stringify(this.images)));
     }
 };
 </script>
